5d04e3c95f82fa4c7ef100907297fd64da56aa35
Dancan Ngare Lumumba <dancanlumumba8@gmail.com>
2025-06-27 12:37:52 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+#Checking if the Database exists
+SELECT 1
+FROM pg_database
+WHERE datname = 'DataWarehouse';
+
+
+create database DataWarehouse;
+
+create schema bronze;
+create schema silver;
+create schema gold;
